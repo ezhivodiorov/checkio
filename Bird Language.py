78e854c0659580c6e4833fcc3360be25b6474454
ezhivodiorov <e.zhivodiorov@gmail.com>
2023-04-25 11:26:41 +0300

    Finish

--- Bird Language.py
+++ Bird Language.py
@@ -1,33 +1,30 @@
 
 def translate(text: str) -> str:
-    letters = 'aeiouy'
+    signt = 'aeiouy'
     newtext = ''
-    propustit = False
-    letter = ''
-
-    for i in text:
-        if propustit and i in letters:
-            propustit = False
-            continue
-        elif i not in letters and i.isalpha():
-            letter = i
-            propustit = True
-        elif letter == i:
-            continue
+    lenght = len(text) -1
+    count = 0
+    while count < lenght:
+        letter = text[count]
+        if letter.isalpha():
+            if letter in signt:
+                count += 3
+            else:
+                count += 2
         else:
-            letter = i
-
+            count += 1
         newtext += letter
+
     return newtext
 
 
 print("Example:")
 print(translate("sooooso aaaaaaaaa"))
 
-# # These "asserts" are used for self-checking
-# assert translate("hieeelalaooo") == "hello"
-# assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin"
-# assert translate("aaa bo cy da eee fe") == "a b c d e f"
-# assert translate("sooooso aaaaaaaaa") == "sos aaa"
-#
-# print("The mission is done! Click 'Check Solution' to earn rewards!")
+# These "asserts" are used for self-checking
+assert translate("hieeelalaooo") == "hello"
+assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin"
+assert translate("aaa bo cy da eee fe") == "a b c d e f"
+assert translate("sooooso aaaaaaaaa") == "sos aaa"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
