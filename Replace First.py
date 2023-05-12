44e5b2337ca544ff351d35312589eff05b2d1366
ezhivodiorov <e.zhivodiorov@gmail.com>
2023-05-12 11:56:00 +0300

    Finall

---
+++ Replace First.py
@@ -0,0 +1,19 @@
+from collections.abc import Iterable
+
+
+def replace_first(items: list) -> Iterable:
+    if len(items) != 0:
+        items.append(items[0])
+        items.pop(0)
+    return items
+
+
+# These "asserts" are used for self-checking
+print("Example:")
+print(list(replace_first([1, 2, 3, 4])))
+
+assert replace_first([1, 2, 3, 4]) == [2, 3, 4, 1]
+assert replace_first([1]) == [1]
+assert replace_first([]) == []
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
