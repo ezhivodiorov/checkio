bbbbcff4de5dbde1411af3713fc9e319369dd11f
ezhivodiorov <e.zhivodiorov@gmail.com>
2023-05-26 14:06:00 +0300

    FINAL!!!

--- Strings Theory/Text Formatting.py
+++ Strings Theory/Text Formatting.py
@@ -1,15 +1,31 @@
 def format_s(text: str, width: int, style: str, ends: bool)->str:
+    quant = width - len(text)
     if style == 'r':
         text = ' '*width+text
         return text[-width:]
     elif style == 'j' and ends == True:
         return text
     elif style == 'c':
-        quant = width - len(text)
         start = int(quant / 2)
         return ' '*start + text
     elif style == 'j':
-        return text
+        newtext = ''
+        lspace = [1 for i in range(text.count(' '))]
+        index = 0
+        while quant > 0:
+            lspace[index] += 1
+            quant -= 1
+            index += 1
+            if index == len(lspace):
+                index = 0
+        lspace.append(0)
+        words = text.split(" ")
+        index = 0
+        for word in words:
+            newtext += word + ' '* lspace[index]
+            index+=1
+
+        return newtext
     else:
         return text
 
@@ -35,19 +51,18 @@ LINE = (
     "et necessitatibus mollitia. Quasi, culpa."
 )
 print("Example:")
-print(text_formatting(LINE, 30, "c"))
-print('-------')
-print(" Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit.\n Iure harum suscipit aperiam\n  aliquam ad, perferendis ex\n     molestias reiciendis\naccusantium quos, tempore sunt\n   quod veniam, eveniet et\n   necessitatibus mollitia.\n        Quasi, culpa.")
+print(text_formatting(LINE, 45, "j"))
+
 
 # These "asserts" are used for self-checking
-# assert (
-#     text_formatting(
-#         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
-#         38,
-#         "l",
-#     )
-#     == "Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit. Iure\nharum suscipit aperiam aliquam ad,\nperferendis ex molestias reiciendis\naccusantium quos, tempore sunt quod\nveniam, eveniet et necessitatibus\nmollitia. Quasi, culpa."
-# )
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        38,
+        "l",
+    )
+    == "Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit. Iure\nharum suscipit aperiam aliquam ad,\nperferendis ex molestias reiciendis\naccusantium quos, tempore sunt quod\nveniam, eveniet et necessitatibus\nmollitia. Quasi, culpa."
+)
 assert (
     text_formatting(
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
@@ -56,21 +71,21 @@ assert (
     )
     == " Lorem ipsum dolor sit amet,\nconsectetur adipisicing elit.\n Iure harum suscipit aperiam\n  aliquam ad, perferendis ex\n     molestias reiciendis\naccusantium quos, tempore sunt\n   quod veniam, eveniet et\n   necessitatibus mollitia.\n        Quasi, culpa."
 )
-# assert (
-#     text_formatting(
-#         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
-#         50,
-#         "r",
-#     )
-#     == "           Lorem ipsum dolor sit amet, consectetur\n     adipisicing elit. Iure harum suscipit aperiam\n   aliquam ad, perferendis ex molestias reiciendis\n       accusantium quos, tempore sunt quod veniam,\n eveniet et necessitatibus mollitia. Quasi, culpa."
-# )
-# assert (
-#     text_formatting(
-#         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
-#         45,
-#         "j",
-#     )
-#     == "Lorem   ipsum  dolor  sit  amet,  consectetur\nadipisicing elit. Iure harum suscipit aperiam\naliquam    ad,   perferendis   ex   molestias\nreiciendis  accusantium  quos,  tempore  sunt\nquod   veniam,   eveniet   et  necessitatibus\nmollitia. Quasi, culpa."
-# )
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        50,
+        "r",
+    )
+    == "           Lorem ipsum dolor sit amet, consectetur\n     adipisicing elit. Iure harum suscipit aperiam\n   aliquam ad, perferendis ex molestias reiciendis\n       accusantium quos, tempore sunt quod veniam,\n eveniet et necessitatibus mollitia. Quasi, culpa."
+)
+assert (
+    text_formatting(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure harum suscipit aperiam aliquam ad, perferendis ex molestias reiciendis accusantium quos, tempore sunt quod veniam, eveniet et necessitatibus mollitia. Quasi, culpa.",
+        45,
+        "j",
+    )
+    == "Lorem   ipsum  dolor  sit  amet,  consectetur\nadipisicing elit. Iure harum suscipit aperiam\naliquam    ad,   perferendis   ex   molestias\nreiciendis  accusantium  quos,  tempore  sunt\nquod   veniam,   eveniet   et  necessitatibus\nmollitia. Quasi, culpa."
+)
 #
 # print("The mission is done! Click 'Check Solution' to earn rewards!")
