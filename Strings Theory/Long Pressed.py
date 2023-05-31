caf74e1ef55094c772ed664703cc1f70d12d549b
ezhivodiorov <e.zhivodiorov@gmail.com>
2023-05-31 09:36:14 +0300

    Chat GPT + me

--- Strings Theory/Long Pressed.py
+++ Strings Theory/Long Pressed.py
@@ -1,20 +1,39 @@
-def compress(text: str):
-    newtext = []
-    prev = None
-    for i in text:
-        if i != prev:
-            newtext.append(i)
-            prev = i
+# def compress(text: str):
+#     newtext = []
+#     prev = None
+#     for i in text:
+#         if i != prev:
+#             newtext.append(i)
+#             prev = i
+#
+#     # return ''.join((newtext.append(i), prev) for i in text if i != prev)
+#     return ''.join(newtext)
+#
+# def long_pressed(text: str, typed: str) -> bool:
+#     return compress(text) == compress(typed)
 
-    # return ''.join((newtext.append(i), prev) for i in text if i != prev)
-    return ''.join(newtext)
+def long_pressed(text, typed):
+    original_index = 0
+    printed_index = 0
 
-def long_pressed(text: str, typed: str) -> bool:
-    return compress(text) == compress(typed)
+    while original_index < len(text) and printed_index < len(typed):
+        if text[original_index] == typed[printed_index]:
+            original_index += 1
+            printed_index += 1
+        elif typed[printed_index] == typed[printed_index - 1]:
+            printed_index += 1
+        else:
+            return False
 
+    if original_index == len(text) and printed_index == len(typed) :
+        return original_index != printed_index
+    elif original_index == len(text) and typed[printed_index] == typed[printed_index - 1]:
+        return True
+    else:
+        return False
 
 print("Example:")
-assert long_pressed('welcome boss!', 'welcooome bos!!') == False
+assert long_pressed("alex", "aaleex") == True
 print(long_pressed("alex", "aaleex"))
 
 # These "asserts" are used for self-checking
