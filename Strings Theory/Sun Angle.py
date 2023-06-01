f65cffaf76c96357488502f82a658cf673e9aab3
Evgeniy <e.zhivodiorov@gmail.com>
2023-06-01 22:54:43 +0300

    Final

--- Strings Theory/Sun Angle.py
+++ Strings Theory/Sun Angle.py
@@ -3,11 +3,11 @@ from typing import Union
 def sun_angle(time: str) -> Union[float, str]:
     h, m  = map(int, time.split(":"))
     angle = h*15 + m*0.25 - 90
-    return angle if angle >= 0 or angle <= 180 else "I don't see the sun!"
+    return angle if angle >= 0 and angle <= 180 else "I don't see the sun!"
 
 
 print("Example:")
-print(sun_angle("18:01"))
+print(sun_angle("05:55"))
 
 assert sun_angle("07:00") == 15
 assert sun_angle("12:15") == 93.75
