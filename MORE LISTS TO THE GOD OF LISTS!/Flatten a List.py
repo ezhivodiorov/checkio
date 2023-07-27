1debf771e179f2670fd2d4336618f4fb0871f1a0
ezhivodiorov <e.zhivodiorov@gmail.com>
2023-07-27 17:22:50 +0300

    1 try

---
+++ MORE LISTS TO THE GOD OF LISTS!/Flatten a List.py
@@ -0,0 +1,26 @@
+from collections.abc import Iterable
+
+
+def flat_list(array: list[int]) -> Iterable[int]:
+    return [int(x) for x in str(array).replace('[', '').replace(']', '').split(',')]
+
+print("Example:")
+print(list(flat_list([1, [2, 2, 2], 4])))
+
+# These "asserts" are used for self-checking
+assert list(flat_list([1, 2, 3])) == [1, 2, 3]
+assert list(flat_list([1, [2, 2, 2], 4])) == [1, 2, 2, 2, 4]
+assert list(flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]])) == [
+    2,
+    4,
+    5,
+    6,
+    6,
+    6,
+    6,
+    6,
+    7,
+]
+assert list(flat_list([-1, [1, [-2], 1], -1])) == [-1, 1, -2, 1, -1]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
